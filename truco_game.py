13be0ba88ebe30e26845f593b15f625df27ffd01
msa190 <mauricio98sousa@gmail.com>
2017-09-12 01:29:44 -0300

    Update truco_game.py

--- truco_game.py
+++ truco_game.py
@@ -34,46 +34,45 @@ class Mao(list):
 	def __str__(self):
 		for i in self.cartas:
 			return i.numero + ' de ' + i.naipe
-class Baralho():
-	cartas = []
+class BaralhoDeTruco(Mao):
+	def __init__(self):
+		#define o zap
+		self.cartas.append(Carta(3,1))
+		self.cartas[-1].valor = 9
 	
-	#define o zap
-	cartas.append(Carta(3,1))
-	cartas[-1].valor = 9
-	
-	#define o sete de copas
-	cartas.append(Carta(6,2))
-	cartas[-1].valor = 8
+		#define o sete de copas
+		self.cartas.append(Carta(6,2))
+		self.cartas[-1].valor = 8
 
-	#define o espadilha
-	cartas.append(Carta(0,1))
-	cartas[-1].valor = 7
+		#define o espadilha
+		self.cartas.append(Carta(0,1))
+		self.cartas[-1].valor = 7
 
-	#define o sete de ouro
-	cartas.append(Carta(6,0))
-	cartas[-1].valor = 6
+		#define o sete de ouro
+		self.cartas.append(Carta(6,0))
+		self.cartas[-1].valor = 6
 	
-	for i in range(13):
-		for j in range(4):
-			if i == 0:
-				if j!= 3:
-					cartas.append(Carta(i,j))
-					cartas[-1].valor = 3
-			if i == 1:
-				cartas.append(Carta(i,j))
-				cartas[-1].valor = 4
-			if i == 2:
-				cartas.append(Carta(i,j))
-				cartas[-1].valor = 5	
-			if i == 10:
-				cartas.append(Carta(i,j))
-				cartas[-1].valor = 1
-			if i == 11:
-				cartas.append(Carta(i,j))
-				cartas[-1].valor = 0
-			if i == 12:
-				cartas.append(Carta(i,j))
-				cartas[-1].valor = 2
+		for i in range(13):
+			for j in range(4):
+				if i == 0:
+					if j!= 3:
+						self.cartas.append(Carta(i,j))
+						self.cartas[-1].valor = 3
+				if i == 1:
+					self.cartas.append(Carta(i,j))
+					self.cartas[-1].valor = 4
+				if i == 2:
+					self.cartas.append(Carta(i,j))
+					self.cartas[-1].valor = 5	
+				if i == 10:
+					self.cartas.append(Carta(i,j))
+					self.cartas[-1].valor = 1
+				if i == 11:
+					self.cartas.append(Carta(i,j))
+					self.cartas[-1].valor = 0
+				if i == 12:
+					self.cartas.append(Carta(i,j))
+					self.cartas[-1].valor = 2
 def combate(a,b):
 	if a>b:
 		print a,"mata",b
@@ -81,7 +80,6 @@ def combate(a,b):
 		print b,"mata",a
 	else:
 		print "Empate"
+baralho = BaralhoDeTruco()
 for i in baralho.cartas:
 	print i, i.valor
-
-baralho = Baralho()
