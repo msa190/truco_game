3eaf34367124a13a799e45823649206ccda06cc5
msa190 <mauricio98sousa@gmail.com>
2017-09-12 01:19:31 -0300

    Update truco_game.py

--- truco_game.py
+++ truco_game.py
@@ -1,22 +1,32 @@
 #encoding: utf8
-_numeros = ['3','2','As','K','J','Q','4','7']
-_naipes=['ouros', 'paus','copas','espadas']
 class Carta:
+	_numeros = ['As','2','3','4','5','6','7','8','9','10','J','Q','K']
+	_naipes=['ouros', 'paus','copas','espadas']
+	valor = 0	
+	numero = None
+	naipe = None
+
 	def __init__(self, a, b):
-		if(a in _numeros and b in _naipes ):
+		if(a in self._numeros and b in self._naipes ):
 			self.numero = a		
 			self.naipe = b
-		if(a in range(len(_numeros)) and b in range(len(_naipes))):
-			self.numero = _numeros[a]
-			self.naipe = _naipes[b]
+		if(a in range(len(self._numeros)) and b in range(len(self._naipes))):
+			self.numero = self._numeros[a]
+			self.naipe = self._naipes[b]
 	def __str__(self):
 		return self.numero + ' de ' + self.naipe
+	
 	def __eq__(self, b):
 		if self.numero == b.numero and self.naipe == b.naipe:
 			return True
 		else:
 			return False
-	valor = 0
+	def __lt__(self,b):
+		if self.valor < b.valor:
+			return True
+	def __gt__(self,b):
+		if self.valor > b.valor:
+			return True
 #class jogador:
 	
 class Mao(list):
@@ -24,49 +34,54 @@ class Mao(list):
 	def __str__(self):
 		for i in self.cartas:
 			return i.numero + ' de ' + i.naipe
-class Baralho(Mao):
+class Baralho():
 	cartas = []
+	
 	#define o zap
-	zap = Carta(6,1)
-	cartas.append(Carta(6,1))
-	Carta(6,1).valor = 9
+	cartas.append(Carta(3,1))
+	cartas[-1].valor = 9
 	
 	#define o sete de copas
-	cartas.append(Carta(7,2))
-	Carta(7,2).valor = 8
+	cartas.append(Carta(6,2))
+	cartas[-1].valor = 8
 
 	#define o espadilha
-	cartas.append(Carta(2,1))
-	Carta(2,1).valor = 7
+	cartas.append(Carta(0,1))
+	cartas[-1].valor = 7
 
 	#define o sete de ouro
-	cartas.append(Carta(7,0))
-	Carta(7,0).valor = 6
+	cartas.append(Carta(6,0))
+	cartas[-1].valor = 6
 	
-	#completar 3 e 2
-	for i,j in enumerate(_numeros[0:2]):
-		for k in _naipes:
-			cartas.append(Carta(j,k))
+	for i in range(13):
+		for j in range(4):
 			if i == 0:
-				#print Carta(j,k), "tem valor 5"
-				Carta(j,k).valor = 5			
+				if j!= 3:
+					cartas.append(Carta(i,j))
+					cartas[-1].valor = 3
 			if i == 1:
-				Carta(j,k).valor = 4
-	#completar As
-	for i,j in enumerate(_numeros[2:3]):
-		for k in _naipes[0:3]:
-			cartas.append(Carta(j,k))
-			Carta(j,k).valor =  3
-	#completar K,J,Q
-	for i,j in enumerate(_numeros[3:6]):
-		for k in _naipes:
-			cartas.append(Carta(j,k))
-			if i == 0:
-				Carta(j,k).valor = 2
-			if i == 1:
-				Carta(j,k).valor = 1
+				cartas.append(Carta(i,j))
+				cartas[-1].valor = 4
 			if i == 2:
-				Carta(j,k).valor = 0
-Baralho = Baralho()
-for i in Baralho.cartas:
+				cartas.append(Carta(i,j))
+				cartas[-1].valor = 5	
+			if i == 10:
+				cartas.append(Carta(i,j))
+				cartas[-1].valor = 1
+			if i == 11:
+				cartas.append(Carta(i,j))
+				cartas[-1].valor = 0
+			if i == 12:
+				cartas.append(Carta(i,j))
+				cartas[-1].valor = 2
+def combate(a,b):
+	if a>b:
+		print a,"mata",b
+	if a<b:
+		print b,"mata",a
+	else:
+		print "Empate"
+for i in baralho.cartas:
 	print i, i.valor
+
+baralho = Baralho()
