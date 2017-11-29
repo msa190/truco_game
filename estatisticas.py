a73d54077d93a5c3552d0c2a9d2a7389e7720b83
msa190 <mauricio98sousa@gmail.com>
2017-11-29 00:02:01 -0200

    correcao do nome de variavel-inteiro counter

--- estatisticas.py
+++ estatisticas.py
@@ -8,7 +8,7 @@ def contains_zap(jogada):
         return False
 
 
-def zap_wins(jogada):
+def zap_win(jogada):
     if ('3|1' in jogada[0:2] and jogada[6] == 'A') or ('3|1' in jogada[3:5] and jogada[6] == 'B'):
         return True
     else:
@@ -17,12 +17,15 @@ def zap_wins(jogada):
 
 zap_count = 0
 zap_wins = 0
-
+process = 0
 with file:
     for line in file:
         jogada = line.split(';')
         
         if contains_zap(jogada):
             zap_count += 1
-        if zap_wins(jogada):
+        if zap_win(jogada):
             zap_wins += 1
+        if process%10000000:
+        	print process
+    	process +=1
